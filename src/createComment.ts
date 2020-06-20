af8977664857c1e1d81b2e308b2d012520268615
KuangPF <1633397286@qq.com>
2020-06-20 15:58:52 +0800

    feat: update command

---
+++ src/createComment.ts
@@ -0,0 +1,10 @@
+/**
+ * 添加头部注释
+ */
+import * as vscode from 'vscode'
+
+const createComment = () => {
+  vscode.window.showInformationMessage('Hello World from vscode-concise-comment!')
+}
+
+export default createComment
